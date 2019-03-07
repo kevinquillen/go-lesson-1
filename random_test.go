fe0824ade9d1cd887dd4c2b7a61a1b928794e1d3
Kevin Quillen <kevinquillen@gmail.com>
2019-03-06 20:07:19 -0500

    Now that we learned a little more, add a test to ensure we cannot make a mistake and pass a maximum value less than the minimum.

--- random_test.go
+++ random_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestRandom(t *testing.T) {
 	min := 5
 	max := 20
-	number := random(min, max)
+	number, _ := random(min, max)
 
 	if number < min {
 		t.Errorf("Number generated was less than the minimum, got: %d. Minimum: %d.", number, min)
@@ -13,5 +13,12 @@ func TestRandom(t *testing.T) {
 
 	if number > max {
 		t.Errorf("Number generated was greater than the maximum, got: %d. Maximum: %d.", number, max)
+	}
+
+	// String testing is probably asinine, how do you do a better 'exception expected' style test?
+	_, err := random(50, 10)
+
+	if err == nil {
+		t.Errorf("Maximum passed is greater than the minimum, an error should be returned, got nil.")
 	}
 }
