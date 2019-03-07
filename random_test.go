8f4fea413d3cf92d27ac2481ed85a17525a904fc
Kevin Quillen <kevinquillen@gmail.com>
2019-03-06 21:46:31 -0500

    Since we are using rand(), we should probably clarify the function purpose instead of using just random().

--- random_test.go
+++ random_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestRandom(t *testing.T) {
 	min := 5
 	max := 20
-	number, _ := random(min, max)
+	number, _ := randomRange(min, max)
 
 	if number < min {
 		t.Errorf("Number generated was less than the minimum, got: %d. Minimum: %d.", number, min)
@@ -16,7 +16,7 @@ func TestRandom(t *testing.T) {
 	}
 
 	// String testing is probably asinine, how do you do a better 'exception expected' style test?
-	_, err := random(50, 10)
+	_, err := randomRange(50, 10)
 
 	if err == nil {
 		t.Errorf("Maximum passed is greater than the minimum, an error should be returned, got nil.")
